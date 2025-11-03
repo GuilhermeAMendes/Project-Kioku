c56525072905763455d5688fdefa5dca9e482d28
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-11-03 16:56:52 -0300

    feat: create service to restore all movies available

---
+++ src/modules/movies/services/get/getAllMovies.ts
@@ -0,0 +1,34 @@
+// Infra
+import { AxiosClient } from "../../../../infra/http/axios/axiosClient";
+import { isAxiosError } from "axios";
+
+// Type Guard
+import { right } from "@shared/patterns/either";
+
+// Factory
+import errorFactory from "@shared/errors/factory/errorFactory";
+
+// Types
+import type { GetAllMoviesResponse } from "@modules/movies/types";
+
+export default async function getAllMovies() {
+  try {
+    const { data: response } = await AxiosClient.get<GetAllMoviesResponse>(
+      "movies"
+    );
+    return right(response);
+  } catch (error) {
+    let message = "Restore movies failed";
+
+    if (isAxiosError(error)) {
+      message =
+        error.response?.data?.detail ||
+        error.response?.data?.message ||
+        error.message;
+    } else if (error instanceof Error) {
+      message = error.message;
+    }
+
+    return errorFactory("not_found", message);
+  }
+}
