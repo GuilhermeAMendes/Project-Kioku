95335d3c7a60ddd351ce4042bfdaaf120c00bdb7
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-11-03 18:32:09 -0300

    feat: create routes management file

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+// External Library
+import { BrowserRouter as Router } from "react-router-dom";
+
+// Routes
+import AppRoutes from "./routes";
+
+// Components
+import { Header } from "./components/structure/Header";
+import { Footer } from "./components/structure/Footer";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold">Hello world!</h1>;
+  return (
+    <Router>
+      <Header />
+      <AppRoutes />
+      <Footer />
+    </Router>
+  );
 }
---
+++ src/routes.tsx
@@ -0,0 +1,34 @@
+// External library
+import { type RouteObject, useRoutes } from "react-router-dom";
+
+const routesList: RouteObject[] = [
+  {
+    path: "/",
+    element: <>Homepage</>,
+  },
+  {
+    path: "/create",
+    element: <>Homepage</>,
+  },
+  {
+    path: "/update",
+    element: <>Homepage</>,
+  },
+  {
+    path: "/delete",
+    element: <>Homepage</>,
+  },
+  {
+    path: "/movie/:id",
+    element: <>Homepage</>,
+  },
+  {
+    path: "*",
+    element: <>Homepage</>,
+  },
+];
+
+export default function AppRoutes() {
+  const elementRoutes = useRoutes(routesList);
+  return elementRoutes;
+}
