684b6a8d747587248683fc9611da4fe6206eefcf
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-11-03 16:07:10 -0300

    chore: configure depencencies of style, handlers and types

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@components": path.resolve(__dirname, "src/components"),
+            "@hooks": path.resolve(__dirname, "src/hooks"),
+            "@services": path.resolve(__dirname, "src/services"),
+            "@utils": path.resolve(__dirname, "src/utils"),
+            "@assets": path.resolve(__dirname, "src/assets"),
+            "@modules": path.resolve(__dirname, "src/modules"),
+            "@shared": path.resolve(__dirname, "src/shared"),
+        },
+    },
+});
