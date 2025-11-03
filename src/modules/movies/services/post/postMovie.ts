92d714e159e54c1e64666c227c32bc7443f20980
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-11-03 17:21:22 -0300

    feat: create service to add a new movie

---
+++ src/modules/movies/services/post/postMovie.ts
@@ -0,0 +1,40 @@
+// Infra
+import { AxiosClient } from "../../../../infra/http/axios/axiosClient";
+import { isAxiosError } from "axios";
+
+// Type Guard
+import { type Either, right } from "@shared/patterns/either";
+
+// Error
+import { ApplicationError } from "@shared/errors/base/ApplicationError";
+
+// Factory
+import errorFactory from "@shared/errors/factory/errorFactory";
+
+// Types
+import type { CreateMoviePayload, CreateMovieResponse } from "../../types";
+
+export default async function postRating(
+  payload: CreateMoviePayload
+): Promise<Either<ApplicationError, CreateMovieResponse>> {
+  try {
+    const { data: response } = await AxiosClient.post<CreateMovieResponse>(
+      "movies",
+      payload
+    );
+    return right(response);
+  } catch (error) {
+    let message = "Restore movies failed";
+
+    if (isAxiosError(error)) {
+      message =
+        error.response?.data?.detail ||
+        error.response?.data?.message ||
+        error.message;
+    } else if (error instanceof Error) {
+      message = error.message;
+    }
+
+    return errorFactory("custom", message);
+  }
+}
