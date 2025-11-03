f24a508aa068791d20f136ad6ea9ee3fb93b6f71
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-11-03 18:14:35 -0300

    feat: create header component

---
+++ src/components/structure/Header/index.tsx
@@ -0,0 +1,16 @@
+// External Library
+import { Link } from "react-router-dom";
+
+export function Header() {
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="flex h-16 items-center justify-between px-6">
+        <Link href="/" className="flex items-center space-x-2">
+          <span className="font-bold text-2xl tracking-tight text-primary">
+            Kioku
+          </span>
+        </Link>
+      </div>
+    </header>
+  );
+}
