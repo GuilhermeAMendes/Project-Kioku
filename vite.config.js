b8c655279c1c05ffbdead5f605a821a7d8648813
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-11-04 12:16:58 -0300

    fix: add missing alias to root

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,18 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            "@components": path.resolve(__dirname, "src/components"),
-            "@hooks": path.resolve(__dirname, "src/hooks"),
-            "@services": path.resolve(__dirname, "src/services"),
-            "@utils": path.resolve(__dirname, "src/utils"),
-            "@assets": path.resolve(__dirname, "src/assets"),
-            "@modules": path.resolve(__dirname, "src/modules"),
-            "@shared": path.resolve(__dirname, "src/shared"),
-            "@lib": path.resolve(__dirname, "src/lib"),
-        },
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@modules": path.resolve(__dirname, "src/modules"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+      "@lib": path.resolve(__dirname, "src/lib"),
     },
+  },
 });
