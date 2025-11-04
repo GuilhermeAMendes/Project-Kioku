994052914c989af0974dc1780d8e8cb8225a2bfe
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-11-04 11:55:53 -0300

    feat: create main container to pages

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,20 @@ import AppRoutes from "./routes";
 import { Header } from "./components/structure/Header";
 import { Footer } from "./components/structure/Footer";
 import { AppNavigation } from "./components/structure/AppNavigation";
+import { Toaster } from "./components/ui/sonner";
 
 export default function App() {
   return (
     <Router>
       <Header />
-      <main>
-        <AppNavigation />
-        <AppRoutes />
+      <main className="flex-1 flex flex-col">
+        <div className="container mx-auto p-4 md:p-8">
+          <AppNavigation />
+          <AppRoutes />
+        </div>
       </main>
       <Footer />
+      <Toaster richColors position="top-right" />
     </Router>
   );
 }
